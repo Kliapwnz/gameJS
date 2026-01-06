915e893f25f238771e46bf098cbf502441a8908d
Evgeniy Klimovich <kliasanik@gmail.com>
2026-01-06 23:04:45 +0300

    appended title

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-
 const playlists = [
   {
-    id:1,
-    title : "Hip-Hop Hits"
+    id: 1,
+    title: "Hip-Hop Hits"
   },
   {
-    id:2,
+    id: 2,
     title: "Rap Hits 1990s"
   }
 ]
 
 const root = document.getElementById("root")
 
-for (let i = 0; i< playlists.length; i++) {
+for (let i = 0; i < playlists.length; i++) {
   const playlist = playlists[i];
 
   const playlistElement = document.createElement('div')
-  const playlistTitleElement =document.createElement("h2")
+  const playlistTitleElement = document.createElement("h2")
   playlistTitleElement.append(playlist.title)
   playlistElement.append(playlistTitleElement)
 

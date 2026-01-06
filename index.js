77e616638337ae6d990b8d222ece632892f2822f
Evgeniy Klimovich <kliasanik@gmail.com>
2026-01-06 23:04:36 +0300

    appended title

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ const playlists = [
     title: "Rap Hits 1990s"
   }
 ]
+
+const root = document.getElementById("root")
+
+for (let i = 0; i< playlists.length; i++) {
+  const playlist = playlists[i];
+
+  const playlistElement = document.createElement('div')
+  const playlistTitleElement =document.createElement("h2")
+  playlistTitleElement.append(playlist.title)
+  playlistElement.append(playlistTitleElement)
+
+  root.append(playlistTitleElement)
+}

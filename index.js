dbd4fa579f52ec5763ed4e20cf61f1be46c70b0a
Evgeniy Klimovich <kliasanik@gmail.com>
2026-01-06 23:21:33 +0300

    create tracks

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const playlists = [
   {
     id: 1,
-    title: "Hip-Hop Hits"
+    title: "Hip-Hop Hits",
+    tracks: [
+      {
+        artist: "Eminem",
+        title: "Rap God",
+        isHot: true
+      },
+      {
+        artist: "50 cent",
+        title: "In da Club",
+        isHot: false
+      }
+    ]
   },
   {
     id: 2,
-    title: "Rap Hits 1990s"
+    title: "Rap Hits 1990s",
+    tracks: [
+      {
+        artist: "Public Enemy",
+        title: "Fight the Power",
+        isHot: true
+      }
+    ]
   }
 ]
 
@@ -18,6 +37,11 @@ for (let i = 0; i < playlists.length; i++) {
   const playlistTitleElement = document.createElement("h2")
   playlistTitleElement.append(playlist.title)
   playlistElement.append(playlistTitleElement)
+  const tracksElement = document.createElement('ul')
+  for (let i = 0; i < playlist.tracks.length; i++) {
+    const element = playlist.tracks[i]
+  }
+
 
   root.append(playlistTitleElement)
 }

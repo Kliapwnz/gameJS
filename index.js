1c774f1bde416809847594a054fb087252eeadfa
Evgeniy Klimovich <kliasanik@gmail.com>
2026-01-07 20:48:56 +0300

    add images and audio

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const playlists = [
         artist: "50 cent",
         title: "In da Club",
         isHot: false,
-        imageSource: "./img/icons/50cent.gpg"
+        imageSource: "./img/icons/50cent.gpeg"
       }
     ]
   },

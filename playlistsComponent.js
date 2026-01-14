02c575f9d21c2bc17ed857934b47fddcbff30430
Evgeniy Klimovich <kliasanik@gmail.com>
2026-01-14 21:31:21 +0300

    remove 2

---
+++ playlistsComponent.js
@@ -0,0 +1,11 @@
+export function playlistsComponent(playlists) {
+  const element = document.createElement('div')
+  for (let i = 0; i < playlists.length; i++) {
+    const playlist = playlists[i];
+
+    element.append(playlistComponent(playlist))
+
+
+  }
+  return element
+}

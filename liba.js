b1cf25d2bbd6678f42f06297ffb6142ff9e0f715
Evgeniy Klimovich <kliasanik@gmail.com>
2026-01-14 21:31:05 +0300

    remove liba

---
+++ liba.js
@@ -0,0 +1,9 @@
+export const liba = {
+  create(tagName, classes = []) {
+    const element = document.createElement(tagName)
+    classes.forEach(c => {
+      element.classList.add(c)
+    })
+    return element
+  }
+}

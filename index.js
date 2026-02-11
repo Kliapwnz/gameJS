c7c6fb18fb74d1fa57d65d017404836193eda2dc
Evgeniy Klimovich <kliasanik@gmail.com>
2026-02-11 13:55:31 +0300

    AddEditPlaylistComponent.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import {playlistsComponent} from "./ui/Playlists/playlistsComponent.js";
 import {playlists} from "./data/data.js";
+import {AddEditPlaylistComponent} from "./ui/AddEditPlaylist/AddEditPlaylistComponent.js";
 
 const root = document.getElementById("root")
 
 
 const playlistsElement = playlistsComponent(playlists)
-root.append(playlistsElement)
+const addEditPlaylistElement = AddEditPlaylistComponent()
+root.append(playlistsElement, addEditPlaylistElement)
 

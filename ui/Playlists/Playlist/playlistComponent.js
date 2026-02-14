310b4816e3f3d2782d695720abdeb7a902f17270
Evgeniy Klimovich <kliasanik@gmail.com>
2026-02-14 23:35:36 +0300

    add button ❌

--- ui/Playlists/Playlist/playlistComponent.js
+++ ui/Playlists/Playlist/playlistComponent.js
@@ -1,11 +1,19 @@
 import {liba} from "../../../shared/liba.js";
 import {TracksComponent} from "./Tracks/tracksComponent.js"
+
 export function playlistComponent(playlist) {
   const element = liba.create('div', ["playlist"])
 
   const playlistTitleElement = document.createElement("h2")
   playlistTitleElement.append(playlist.title)
-  element.append(playlistTitleElement, TracksComponent(playlist.tracks))
+
+  const deleteButtonElement = liba.create('button')
+  deleteButtonElement.append("❌")
+  deleteButtonElement.addEventListener('click', () => {
+    alert("delete")
+  })
+
+  element.append(deleteButtonElement,playlistTitleElement, TracksComponent(playlist.tracks))
   return element
 
 }

c0b70c38eea89becd68199e6b90ad6969409dbd7
Evgeniy Klimovich <kliasanik@gmail.com>
2026-02-24 19:44:49 +0300

    look better

--- data/data.js
+++ data/data.js
@@ -1,25 +1,33 @@
 export let playlists = [{
-  id: 1, title: "Hip-Hop Hits", tracks: [{
-    artist: "Eminem",
-    title: "Rap God",
-    isHot: true,
-    imageSource: "./img/icons/eminem.jpg",
-    audioSource: "./music/Eminem - Rap God.mp3"
-  }, {
-    artist: "50 cent",
-    title: "In da Club",
-    isHot: false,
-    imageSource: "./img/icons/50cent.jpeg",
-    audioSource: "./music/50cent - In da club.mp3"
-  }]
+  id: 1,
+  title: "Hip-Hop Hits",
+  tracks: [
+    {
+      artist: "Eminem",
+      title: "Rap God",
+      isHot: true,
+      imageSource: "./img/icons/eminem.jpg",
+      audioSource: "./music/Eminem - Rap God.mp3"
+    }, {
+      artist: "50 cent",
+      title: "In da Club",
+      isHot: false,
+      imageSource: "./img/icons/50cent.jpeg",
+      audioSource: "./music/50cent - In da club.mp3"
+    }
+  ]
 }, {
-  id: 2, title: "Rap Hits 1990s", tracks: [{
-    artist: "Public Enemy",
-    title: "Fight the Power",
-    isHot: true,
-    imageSource: "./img/icons/PublicEnemy.jpg",
-    audioSource: "./music/Public Enemy - Fight The Power (Remix).mp3"
-  }]
+  id: 2,
+  title: "Rap Hits 1990s",
+  tracks: [
+    {
+      artist: "Public Enemy",
+      title: "Fight the Power",
+      isHot: true,
+      imageSource: "./img/icons/PublicEnemy.jpg",
+      audioSource: "./music/Public Enemy - Fight The Power (Remix).mp3"
+    }
+  ]
 }]
 
 export const deletePlaylist = (id) => {

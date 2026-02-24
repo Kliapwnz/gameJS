b1d9e4bb57328e18ccc7c4918d11e31945493564
Evgeniy Klimovich <kliasanik@gmail.com>
2026-02-24 20:52:43 +0300

    added all

--- ui/AddEditPlaylist/AddEditPlaylistComponent.js
+++ ui/AddEditPlaylist/AddEditPlaylistComponent.js
@@ -1,15 +1,15 @@
 import {liba} from "../../shared/liba.js";
 import {dialogOpen} from "../../data/data.js";
-import {DialogHeaderComponent} from "./DialogHeaderComponent";
-import {DialogFormComponent} from "./DialogFormComponent";
-import {DialogActionsComponent} from "./DialogActionsComponent";
+import {DialogHeaderComponent} from "./DialogHeaderComponent.js";
+import {DialogFormComponent} from "./DialogFormComponent.js";
+import {DialogActionsComponent} from "./DialogActionsComponent.js";
 
 
 export function AddEditPlaylistComponent() {
 
   const element = liba.create('dialog')
 
-  element.append(DialogHeaderComponent, DialogFormComponent, DialogActionsComponent)
+  element.append(DialogHeaderComponent(), DialogFormComponent(), DialogActionsComponent())
   element.open = dialogOpen.value
 
   return element

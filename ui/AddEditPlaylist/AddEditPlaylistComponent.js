f679aab2fe5da8c4830651fbad0e9147c68c5f99
Evgeniy Klimovich <kliasanik@gmail.com>
2026-02-24 19:49:33 +0300

    false element

--- ui/AddEditPlaylist/AddEditPlaylistComponent.js
+++ ui/AddEditPlaylist/AddEditPlaylistComponent.js
@@ -6,7 +6,7 @@ export function AddEditPlaylistComponent() {
   const element = liba.create('dialog')
 
   element.innerHTML = ' <p>Greetings, one and all!</p> <form method="dialog"> <button>OK</button> </form>'
-  element.open = true
+  element.open = false
 
   return element
 }

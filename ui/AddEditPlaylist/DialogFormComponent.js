4867ba57f75b176fec601843be3ba5c3652c7a2a
Evgeniy Klimovich <kliasanik@gmail.com>
2026-02-24 21:45:32 +0300

    timeout added

--- ui/AddEditPlaylist/DialogFormComponent.js
+++ ui/AddEditPlaylist/DialogFormComponent.js
@@ -4,12 +4,16 @@ import {addPlaylistState, setNewTitle} from "../../data/data.js";
 export function DialogFormComponent() {
   const element = liba.create('form')
   const playlistNameInputElement = liba.create('input')
-  playlistNameInputElement.addEventListener('change', () => {
+  playlistNameInputElement.addEventListener('input', () => {
     setNewTitle(playlistNameInputElement.value)
   })
   // const playlistImageSourceElement = liba.create('input')
 
   playlistNameInputElement.value = addPlaylistState.newTitle
+  setTimeout(()=> {
+    playlistNameInputElement.focus()
+  }, 1)
+
   element.append(playlistNameInputElement)// playlistImageSourceElement
 
   return element

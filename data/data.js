6d4190cc41551327751596c0c6146323117853e5
Evgeniy Klimovich <kliasanik@gmail.com>
2026-02-24 22:12:33 +0300

    editor works

--- data/data.js
+++ data/data.js
@@ -1,39 +1,29 @@
 export let playlists = [{
-  id: 1,
-  title: "Hip-Hop Hits",
-  tracks: [
-    {
-      artist: "Eminem",
-      title: "Rap God",
-      isHot: true,
-      imageSource: "./img/icons/eminem.jpg",
-      audioSource: "./music/Eminem - Rap God.mp3"
-    }, {
-      artist: "50 cent",
-      title: "In da Club",
-      isHot: false,
-      imageSource: "./img/icons/50cent.jpeg",
-      audioSource: "./music/50cent - In da club.mp3"
-    }
-  ]
+  id: 1, title: "Hip-Hop Hits", tracks: [{
+    artist: "Eminem",
+    title: "Rap God",
+    isHot: true,
+    imageSource: "./img/icons/eminem.jpg",
+    audioSource: "./music/Eminem - Rap God.mp3"
+  }, {
+    artist: "50 cent",
+    title: "In da Club",
+    isHot: false,
+    imageSource: "./img/icons/50cent.jpeg",
+    audioSource: "./music/50cent - In da club.mp3"
+  }]
 }, {
-  id: 2,
-  title: "Rap Hits 1990s",
-  tracks: [
-    {
-      artist: "Public Enemy",
-      title: "Fight the Power",
-      isHot: true,
-      imageSource: "./img/icons/PublicEnemy.jpg",
-      audioSource: "./music/Public Enemy - Fight The Power (Remix).mp3"
-    }
-  ]
+  id: 2, title: "Rap Hits 1990s", tracks: [{
+    artist: "Public Enemy",
+    title: "Fight the Power",
+    isHot: true,
+    imageSource: "./img/icons/PublicEnemy.jpg",
+    audioSource: "./music/Public Enemy - Fight The Power (Remix).mp3"
+  }]
 }]
 
 export let addEditPlaylistState = {
-  isActive: false,
-  newTitle: "",
-  id: null
+  isActive: false, newTitle: "", id: null
 }
 
 
@@ -46,9 +36,7 @@ export const activateAddEditPlaylist = (playlistId = null) => {
   addEditPlaylistState.isActive = true
   if (playlistId) {
     addEditPlaylistState.id = playlistId
-    const foundPlaylist = playlists.find((p) =>
-      p.id === playlistId
-    )
+    const foundPlaylist = playlists.find((p) => p.id === playlistId)
     if (!foundPlaylist) {
       throw new Error("404")
     }
@@ -61,7 +49,8 @@ export const activateAddEditPlaylist = (playlistId = null) => {
 }
 export const deactivateAddEditPlaylist = () => {
   addEditPlaylistState.isActive = false
-
+  addEditPlaylistState.id = null
+  addEditPlaylistState.newTitle = ""
   emit()
 }
 
@@ -74,13 +63,19 @@ export const createUpdatePlaylist = () => {
   if (addEditPlaylistState.newTitle === '') {
     throw new Error("Empty title")
   }
-  playlists.push({
-    id: Date.now(),
-    title: addEditPlaylistState.newTitle,
-    tracks: []
-  })
+  if (addEditPlaylistState.id) {
+    const foundPlaylist = playlists.find((p) => p.id === addEditPlaylistState.id)
+    if (!foundPlaylist) {
+      throw new Error("404")
+    }
+    foundPlaylist.title = addEditPlaylistState.newTitle
+  } else {
+    playlists.push({
+      id: Date.now(), title: addEditPlaylistState.newTitle, tracks: []
+    })
+
+  }
   deactivateAddEditPlaylist()
-  addEditPlaylistState.newTitle = ""
   emit()
 
 }
